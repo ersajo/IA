9b0aa77dd4e34aa802011f1c59decb073a606de7
Erick <sanchezjoserick@gmail.com>
2017-11-03 15:23:22 -0600

    Agregar F3 para salir, reduccion de tamaño

--- Prac1/Prac1.py
+++ Prac1/Prac1.py
@@ -2,17 +2,17 @@ import pygame, random
 
 class Escenario:
 
-    Dimensiones = (750,900)
+    Dimensiones = (375,450)
     Pantalla = pygame.display.set_mode(Dimensiones)
     World = []
 
     def __init__(self,viewX, viewY):
-        self.Dimensiones = (viewX*50, viewY*50)
+        self.Dimensiones = (viewX*25, viewY*25)
         Pantalla = pygame.display.set_mode(self.Dimensiones)
-        pygame.draw.rect(Pantalla, (255, 0, 255), [0,0,50,50], 0)
+        pygame.draw.rect(Pantalla, (255, 0, 255), [0,0,25,25], 0)
 
     def repaintCharacter(self, X, Y, color):
-        pygame.draw.rect(self.Pantalla, color, [X,Y,50,50], 0)
+        pygame.draw.rect(self.Pantalla, color, [X,Y,25,25], 0)
 
     def getDimensiones(self):
         return self.Dimensiones
@@ -26,11 +26,11 @@ class Escenario:
         for line in contenido:
             for car in line:
                 if car == '0':
-                    pygame.draw.rect(self.Pantalla, AZUL, [X,Y,50,50], 0)
+                    pygame.draw.rect(self.Pantalla, AZUL, [X,Y,25,25], 0)
                 elif car == '1':
-                    pygame.draw.rect(self.Pantalla, VERDE, [X,Y,50,50], 0)
-                X = X + 50
-            Y = Y + 50
+                    pygame.draw.rect(self.Pantalla, VERDE, [X,Y,25,25], 0)
+                X = X + 25
+            Y = Y + 25
             X = 0
 
     def askUP(self,charX,charY):
@@ -78,16 +78,16 @@ class Character:
         return self.Y
 
     def UP(self):
-        self.Y = self.Y-50
+        self.Y = self.Y-25
 
     def DOWN(self):
-        self.Y = self.Y+50
+        self.Y = self.Y+25
 
     def RIGHT(self):
-        self.X = self.X+50
+        self.X = self.X+25
 
     def LEFT(self):
-        self.X = self.X-50
+        self.X = self.X-25
 
 
 texto = Archivo()
@@ -108,20 +108,22 @@ while not Terminar:
        if Evento.type == pygame.QUIT:
             Terminar = True
     #---La logica del juego
+    if(pygame.key.get_pressed()[pygame.K_F3] != 0):
+        break
     if(pygame.key.get_pressed()[pygame.K_UP] != 0):
-        if(monito.getY > 0 and view.askUP(monito.getX/50, monito.getY/50) == '1'):
+        if(monito.getY > 0 and view.askUP(monito.getX/25, monito.getY/25) == '1'):
             view.repaintCharacter(monito.getX, monito.getY,NEGRO)
             monito.UP()
     if(pygame.key.get_pressed()[pygame.K_DOWN] != 0):
-        if(monito.getY+50 < view.getDimensiones()[1] and view.askDOWN(monito.getX/50, monito.getY/50) == '1'):
+        if(monito.getY+25 < view.getDimensiones()[1] and view.askDOWN(monito.getX/25, monito.getY/25) == '1'):
             view.repaintCharacter(monito.getX, monito.getY,NEGRO)
             monito.DOWN()
     if(pygame.key.get_pressed()[pygame.K_RIGHT] != 0):
-        if(monito.getX+50 < view.getDimensiones()[0] and view.askRIGHT(monito.getX/50, monito.getY/50) == '1'):
+        if(monito.getX+25 < view.getDimensiones()[0] and view.askRIGHT(monito.getX/25, monito.getY/25) == '1'):
             view.repaintCharacter(monito.getX, monito.getY,NEGRO)
             monito.RIGHT()
     if(pygame.key.get_pressed()[pygame.K_LEFT] != 0):
-        if(monito.getX > 0 and view.askLEFT(monito.getX/50, monito.getY/50) == '1'):
+        if(monito.getX > 0 and view.askLEFT(monito.getX/25, monito.getY/25) == '1'):
             view.repaintCharacter(monito.getX, monito.getY,NEGRO)
             monito.LEFT()
     #--Todos los dibujos van despues de esta linea
